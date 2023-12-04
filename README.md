fe46857b91f83c05e1653edba3750a165a44fc09
João Victor <galvao845@gmail.com>
2023-12-04 15:47:59 -0300

    V1.0
    subindo base do projeto

--- README.md
+++ README.md
@@ -1,1 +1,179 @@
-# go-advanced-starter
+<h1 align="center"> Go Advanced Starter </h1>
+
+<p align="center"> <strong>Advanced Assistant for Initializing Go Projects</strong> </p>
+
+[![MIT license](https://img.shields.io/badge/license-MIT-brightgreen.svg)](https://opensource.org/licenses/MIT)
+
+
+<h2 align="center"> <strong>Uso</strong> </h2>
+Este script em bash tem como objetivo facilitar a criação de novos projetos em Go (Golang), oferecendo uma estrutura de diretórios inicial e a instalação de bibliotecas comumente utilizadas.
+
+<h2 align="center"> <strong>Estruturas de Projeto</strong> </h2>
+
+Existem duas estruturas principais de projeto suportadas:
+```
+service: Define o projeto como um serviço, seguindo a estrutura de microserviço.
+library: Define o projeto como uma biblioteca.
+```
+
+<h2 align="center"> <strong>Bibliotecas Disponíveis</strong> </h2>
+
+O script oferece a instalação de várias bibliotecas, categorizadas da seguinte forma:
+
+```
+# Logging
+
+github.com/fonteeboa/go-log-guardian.
+github.com/sirupsen/logrus.
+
+# Banco de Dados Relacional
+
+github.com/go-sql-driver/mysql.
+gorm.io/driver/postgres.
+github.com/jmoiron/sqlx.
+
+# Banco de Dados Não Relacional
+
+go.mongodb.org/mongo-driver/mongo.
+
+# Frameworks Web
+
+github.com/gin-gonic/gin.
+github.com/labstack/echo.
+
+# Biblioteca de Mapeamento Objeto-Relacional (ORM)
+
+github.com/go-gorm/gorm.
+
+# Biblioteca de Manipulação de Configurações
+
+github.com/spf13/viper.
+
+# Roteamento
+
+github.com/spf13/cobra.
+github.com/gorilla/mux.
+
+# Autenticação e Segurança
+
+github.com/dgrijalva/jwt-go.
+github.com/appleboy/gin-jwt.
+
+# Testes
+
+github.com/stretchr/testify.
+github.com/onsi/ginkgo.
+
+# Bibliotecas de Autenticação de Múltiplos Fatores (MFA)
+
+github.com/authy/authy-go
+github.com/duosecurity/duo_api_golang
+github.com/okta/okta-sdk-golang
+github.com/dgryski/dgoogauth
+github.com/Azure-Samples/active-directory-go-native-multitenant-v2
+github.com/pquerna/otp/totp
+
+# Outras Bibliotecas Úteis
+
+github.com/go-redis/redis/v8.
+github.com/hashicorp/golang-lru.
+gonum.org/v1/gonum.
+github.com/asaskevich/govalidator.
+github.com/gorilla/websocket.
+github.com/aws/aws-sdk-go.
+google.golang.org/grpc.
+```
+
+<h2 align="center"> <strong>Uso</strong> </h2>
+
+O script pode ser usado da seguinte maneira:
+
+```
+bash
+./create_go.sh PROJECT_NAME [OPTIONS]
+PROJECT_NAME: Nome do projeto a ser criado.
+```
+
+## Exemplo de Uso
+```
+./create_go.sh project_name --mysql --gin --echo
+# Este comando criará um novo projeto chamado project_name como um serviço (microserviço) e instalará os pacotes github.com/gin-gonic/gin e github.com/go-sql-driver/mysql.
+```
+
+## Mensagem Helpers
+
+```
+$ ./create_go.sh --help
+Usage: ./create_go.sh PROJECT_NAME [OPTIONS]
+Creates a new Go project with folder structure and some pre-installed libraries.
+
+OPTIONS:
+
+    --help              Displays this help text
+
+    STRUCTURES:
+        --service           Defines the project as a service (microservice structure)
+        --library           Defines the project as a library (library structure)
+
+    LIBRARIES:
+        Logging:
+            --logg Installs the package github.com/fonteeboa/go-log-guardian
+            --logrus Installs the package github.com/sirupsen/logrus
+
+        Relational Database:
+            --mysql Installs the package github.com/go-sql-driver/mysql
+            --postgres Installs the package gorm.io/driver/postgres
+            --sqlx Installs the package github.com/jmoiron/sqlx
+
+        No Relational Database:
+            --mongodriver Installs the package go.mongodb.org/mongo-driver/mongo
+
+        Web Framework:
+            --gin Installs the package github.com/gin-gonic/gin
+            --echo Installs the package github.com/labstack/echo
+
+        Object-Relational Mapping (ORM) Library:
+            --gorm Installs the package github.com/go-gorm/gorm
+
+        Configuration Handling Library:
+            --viper Installs the package github.com/spf13/viper
+
+        Routing:
+            --cobra Installs the package github.com/spf13/cobra
+            --mux Installs the package github.com/gorilla/mux
+
+        Authentication and Security:
+            --jwtgo Installs the package github.com/dgrijalva/jwt-go
+            --ginjwt Installs the package github.com/appleboy/gin-jwt
+
+        Testing:
+            --testify Installs the package github.com/stretchr/testify
+            --ginkgo Installs the package github.com/onsi/ginkgo
+
+        Multi-Factor Authentication (MFA) Libraries:
+            --mfa-authy Installs the Authy library (Twilio's MFA solution)
+            --mfa-duo Installs the Duo Security library (MFA provider)
+            --mfa-okta Installs the Okta library (Identity and Access Management with MFA support)
+            --mfa-google Installs the Google Authenticator library (Google's MFA solution)
+            --mfa-azure Installs the Azure Multi-Factor Authentication library (Microsoft's MFA solution)
+            --mfa-otp Installs the TOTP library (Time-based One-Time Password)
+
+        Other Useful:
+            --goredis Installs the package github.com/go-redis/redis/v8
+            --golang-lru Installs the package github.com/hashicorp/golang-lru
+            --gonum Installs the package gonum.org/v1/gonum
+            --govalidator Installs the package github.com/asaskevich/govalidator
+            --gorillaw Installs the package github.com/gorilla/websocket
+            --aws Installs the package github.com/aws/aws-sdk-go
+            --grpc Installs the package google.golang.org/grpc
+
+Example usage:
+  ./create_go.sh project_name --mysql --gin --echo
+  This will create a new project 'project_name' as a service (microservice) and install the packages 'github.com/gin-gonic/gin' and 'github.com/go-sql-driver/mysql'.
+```
+
+<h2 align="center"> <strong>Observações</strong> </h2>
+
+Lembre-se de adaptar o projeto criado para se adequar à sua realidade!
+
+As bibiliotecas atuais foram escolhidas por serem as mais comuns em projetos que crio para estudos os quais em sua grande maioria não estão presentes no github.
---
+++ create_go.sh
@@ -0,0 +1,278 @@
+#!/bin/bash
+
+# Function to display help
+show_help() {
+    cat <<EOF
+Usage: $0 PROJECT_NAME [OPTIONS]
+Creates a new Go project with folder structure and some pre-installed libraries.
+
+OPTIONS:
+
+    --help              Displays this help text
+
+    STRUCTURES:
+        --service           Defines the project as a service (microservice structure)
+        --library           Defines the project as a library (library structure)
+
+    LIBRARIES:
+        Logging:
+            --logg Installs the package github.com/fonteeboa/go-log-guardian
+            --logrus Installs the package github.com/sirupsen/logrus
+
+        Relational Database:
+            --mysql Installs the package github.com/go-sql-driver/mysql
+            --postgres Installs the package gorm.io/driver/postgres
+            --sqlx Installs the package github.com/jmoiron/sqlx
+        
+        No Relational Database:
+            --mongodriver Installs the package go.mongodb.org/mongo-driver/mongo
+        
+        Web Framework:
+            --gin Installs the package github.com/gin-gonic/gin
+            --echo Installs the package github.com/labstack/echo
+
+        Object-Relational Mapping (ORM) Library:
+            --gorm Installs the package github.com/go-gorm/gorm
+
+        Configuration Handling Library:
+            --viper Installs the package github.com/spf13/viper
+
+        Routing:
+            --cobra Installs the package github.com/spf13/cobra
+            --mux Installs the package github.com/gorilla/mux
+
+        Authentication and Security:
+            --jwtgo Installs the package github.com/dgrijalva/jwt-go
+            --ginjwt Installs the package github.com/appleboy/gin-jwt
+
+        Testing:
+            --testify Installs the package github.com/stretchr/testify
+            --ginkgo Installs the package github.com/onsi/ginkgo
+
+        Multi-Factor Authentication (MFA):
+            --mfa-authy Installs the Authy package (Twilio's MFA solution)
+            --mfa-duo Installs the Duo Security package (MFA provider)
+            --mfa-okta Installs the Okta package (Identity and Access Management with MFA support)
+            --mfa-google Installs the Google Authenticator package (Google's MFA solution)
+            --mfa-azure Installs the Azure Multi-Factor Authentication package (Microsoft's MFA solution)
+            --mfa-otp Installs the TOTP package (Time-based One-Time Password)
+
+        Other Useful:
+            --goredis Installs the package github.com/go-redis/redis/v8
+            --golang-lru Installs the package github.com/hashicorp/golang-lru
+            --gonum Installs the package gonum.org/v1/gonum
+            --govalidator Installs the package github.com/asaskevich/govalidator
+            --gorillaw Installs the package github.com/gorilla/websocket
+            --aws Installs the package github.com/aws/aws-sdk-go
+            --grpc Installs the package google.golang.org/grpc
+
+Example usage:
+  $0 project_name --mysql --gin --echo
+  This will create a new project 'project_name' as a service (microservice) and install the packages 'github.com/gin-gonic/gin' and 'github.com/go-sql-driver/mysql'.
+EOF
+}
+
+# Function to create project directories
+create_project_directories() {
+    local project_type="$1"
+
+    if [ "$project_type" = "service" ]; then
+        echo "Creating service project..."
+        mkdir -p cmd
+        mkdir -p internal/helpers
+        mkdir -p internal/infra/database
+        mkdir -p internal/infra/http/routes
+        mkdir -p internal/infra/providers
+        mkdir -p internal/modules/module1/{controller,domain,repositories,services,test/mock}
+    else
+        echo "Creating library project..."
+        mkdir -p internal/service
+        mkdir -p pkg
+    fi
+
+    touch README.md
+}
+
+# Install selected libraries
+install_libraries() {
+    local libraries=("$@")
+
+    for lib in "${libraries[@]}"; do
+        case $lib in
+            "--mysql")
+                go get -u github.com/go-sql-driver/mysql
+                echo "MYSQL_HOST=" >> .env
+                echo "MYSQL_PORT=" >> .env
+                echo "MYSQL_USER=" >> .env
+                echo "MYSQL_PASSWORD=" >> .env
+                echo "MYSQL_DBNAME=" >> .env         
+                ;;
+            "--postgres")
+                go get -u gorm.io/driver/postgres
+                echo "POSTGRES_HOST=" >> .env
+                echo "POSTGRES_EXTERNAL_PORT=" >> .env
+                echo "POSTGRES_USER=" >> .env
+                echo "POSTGRES_PASSWORD=" >> .env
+                echo "POSTGRES_DB=" >> .env
+                ;;
+            "--logg")
+                go get -u github.com/fonteeboa/go-log-guardian
+                echo "LOG_GUARDIAN_DATABASE_TYPE=" >> .env
+                ;;
+            "--gin")
+                go get -u github.com/gin-gonic/gin
+                ;;
+            "--echo")
+                go get -u github.com/labstack/echo
+                ;;
+            "--gorm")
+                go get -u github.com/go-gorm/gorm
+                ;;
+            "--viper")
+                go get -u github.com/spf13/viper
+                ;;
+            "--cobra")
+                go get -u github.com/spf13/cobra
+                ;;
+            "--mux")
+                go get -u github.com/gorilla/mux
+                ;;
+            "--logrus")
+                go get -u github.com/sirupsen/logrus
+                ;;
+            "--sqlx")
+                go get -u github.com/jmoiron/sqlx
+                ;;
+            "--jwtgo")
+                go get -u github.com/dgrijalva/jwt-go
+                ;;
+            "--goredis")
+                go get -u github.com/go-redis/redis/v8
+                ;;
+            "--testify")
+                go get -u github.com/stretchr/testify
+                ;;
+            "--ginjwt")
+                go get -u github.com/appleboy/gin-jwt
+                ;;
+            "--ginkgo")
+                go get -u github.com/onsi/ginkgo
+                ;;
+            "--golang-lru")
+                go get -u github.com/hashicorp/golang-lru
+                ;;
+            "--gonum")
+                go get -u gonum.org/v1/gonum
+                ;;
+            "--govalidator")
+                go get -u github.com/asaskevich/govalidator
+                ;;
+            "--gorillaw")
+                go get -u github.com/gorilla/websocket
+                ;;
+            "--mongodriver")
+                go get -u go.mongodb.org/mongo-driver/mongo
+                echo "MONGODB_URI=" >> .env
+                echo "MONGODB_DBNAME=" >> .env
+                ;;
+            "--aws")
+                go get -u github.com/aws/aws-sdk-go
+                ;;
+            "--grpc")
+                go get -u google.golang.org/grpc
+                ;;
+            "--grpc")
+                go get -u google.golang.org/grpc
+                touch .env
+                ;;
+            "--mfa-authy")
+                go get -u github.com/authy/authy-go
+                ;;
+            "--mfa-duo")
+                go get -u github.com/duosecurity/duo_api_golang
+                ;;
+            "--mfa-okta")
+                go get -u github.com/okta/okta-sdk-golang
+                ;;
+            "--mfa-google")
+                go get -u github.com/dgryski/dgoogauth
+                ;;
+            "--mfa-azure")
+                go get -u github.com/Azure-Samples/active-directory-go-native-multitenant-v2
+                ;;
+            "--mfa-otp")
+                go get -u github.com/pquerna/otp/totp
+                ;;
+            "help")
+                show_help
+                exit 0
+                ;;
+            *)
+                echo "Invalid library option: $lib"
+                exit 1
+                ;;
+        esac
+    done
+}
+
+# Variable initialization
+project_name=""
+project_type="library"
+selected_libraries=()
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        --service)
+            project_type="service"
+            shift
+            ;;
+        --library)
+            project_type="library"
+            shift
+            ;;
+        --logg|--mysql|--gin|--echo|--gorm|--viper|--cobra|--mux|--logrus|--sqlx|--jwtgo|--goredis|--testify|--ginjwt|--ginkgo|--golang-lru|--gonum|--govalidator|--gorillaw|--mongodriver|--aws|--grpc|--postgres)
+            selected_libraries+=("$1")
+            shift
+            ;;
+        --help)
+            show_help
+            exit 0
+            ;;
+        *)
+            if [ -z "$project_name" ]; then
+                project_name="$1"
+                shift
+            fi
+        ;;
+    esac
+done
+
+# Check for a provided project name
+if [ -z "$project_name" ]; then
+    echo "A project name is required."
+    show_help
+    exit 1
+fi
+
+# Create project directory
+mkdir -p "$project_name"
+cd "$project_name" || exit
+
+# Initialize a new Go module
+go mod init "$project_name"
+
+# Create project structure
+create_project_directories "$project_type"
+
+# Install selected libraries
+install_libraries "${selected_libraries[@]}"
+
+# Display success message
+cat <<EOF
+Project "$project_name" created successfully!
+Remember to adapt the created project to fit your reality!
+EOF
+
